6b774a77af6a86e47d44e0b9826300027670dbe9
chengang <chengang@sogou-inc.com>
2014-07-02 11:46:12 +0800

    modify uuidutil

--- passport-admin-common/src/main/java/com/sogou/upd/passport/admin/common/utils/UuidUtil.java
+++ passport-admin-common/src/main/java/com/sogou/upd/passport/admin/common/utils/UuidUtil.java
@@ -37,10 +37,4 @@ public final class UuidUtil {
         return StringUtils.splitByWholeSeparator(generateUuid(), STR_SPLIT)[0];
     }
 
-    public static void main(String args[]) {
-        String uuid = UuidUtil.generateUuid();
-        System.out.println(JsonUtil.obj2Json(uuid)); //dfb2c068-6321-472c-b58f-4dba98c193e1 4bc23d02-df09-454d-ad18-3b8632cc050a
-        System.out.println(JsonUtil.obj2Json(generatePassword()));//7790e2a4
-    }
-
 }
